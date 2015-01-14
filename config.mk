854754d94dd91eb677d44ce565f3e35f576b99c1
prost p <prost_p@epitech.eu>
2015-01-14 21:40:14 +0100

    fix

--- config.mk
+++ config.mk
@@ -7,16 +7,19 @@ CFLAGS:=	-march=${ARCH}\
 		-ffreestanding\
 		-O2\
 		-Wall -Wextra -Werror\
-		-nostdlib\
-		-lgcc
 
 AS:=		/usr/i686-elf-4.9.1-Linux-x86_64/bin/i686-elf-as
 ASFLAGS:=
 
 LD:=		/usr/i686-elf-4.9.1-Linux-x86_64/bin/i686-elf-gcc
-LDFLAGS:=	-march=i386 -ffreestanding -O2 -nostdlib -lgcc
+LDFLAGS:=	-march=${ARCH}\
+		-ffreestanding\
+		-O2\
+		-nostdlib -lgcc
 
-BUILDD:=	build
+MAKE:= 		@make --no-print-directory -C
+
+BUILDD:=	build/${ARCH}
 
 QEMU:=		qemu-i386
 
